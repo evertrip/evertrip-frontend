fdcf7510380a5499bad8b46518b49f87cccb5c43
hyeminLapTop <gpals5987@gmail.com>
2024-05-08 22:23:11 +0900

    [feat] 자체 로그인 삭제 및 네이버 로그인 버튼 추가 #4

--- src/modules/login/LoginDisplay.tsx
+++ src/modules/login/LoginDisplay.tsx
@@ -1,34 +1,16 @@
 import React from "react";
-import { TextField, Button, Checkbox, Link } from "@mui/material";
-import AppleIcon from '@mui/icons-material/Apple';
-import GoogleIcon from '@mui/icons-material/Google';
+import {Button, Box} from "@mui/material";
 
 export default function LoginDisplay() {
     return (
       <>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '400px', width: '100%' }}>
-          <h1 style={{ textAlign: 'center' }}>EVERTRIP</h1>
-          <TextField label="Email" type="email" variant="outlined" fullWidth margin="normal" />
-          <TextField label="Password" type="password" variant="outlined" fullWidth margin="normal" />
-
-          
-          <Button variant="contained" color="primary" fullWidth style={{ margin: '8px 0' }}>
-            Sign In
-          </Button>
-          <div style={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
-            <Button variant="outlined" startIcon={<GoogleIcon />} fullWidth>
-              Sign in with Google
-            </Button>
-            <Button variant="outlined" startIcon={<AppleIcon />} fullWidth>
-              Sign in with Apple
-            </Button>
-          </div>
-          <div style={{ textAlign: 'center', marginTop: '20px' }}>
-            Don't have an account? <Link href="/signup">Sign up</Link>
-          </div>
-        </div>
-      </div>
+        <Box style={{ width: '100vn' , height: '80vh' }}>
+          <Box style={{ position: 'absolute', top: '45%', left: '50%', transform: 'translate(-50%, -40%)', width: '400px' }}>
+                  <h1 style={{ textAlign: 'center', fontSize: '50px' }}>EVERTRIP</h1>
+                  <img src="/images/loginImages/loginIconG.png"
+                  style = {{width: '100%'}}/>
+              </Box>
+          </Box>
       </>
     );
 }
