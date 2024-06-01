6bdca051f6ea292f134d9b68ca819b103b5c82db
hyeminLapTop <gpals5987@gmail.com>
2024-06-02 01:24:57 +0900

    [feat] Tag 목록 삽입 #23

--- src/modules/main/seachComponent/SearchBoxForTag.tsx
+++ src/modules/main/seachComponent/SearchBoxForTag.tsx
@@ -1,35 +1,51 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import CustomAxios from '../../config/CustomAxios';
+
+interface Tag {
+  name: string;
+  id: number;
+}
 
 interface SearchBoxForTagProps {
   setTags: React.Dispatch<React.SetStateAction<string[]>>;
-  handleEnterKeyPress: (event: React.KeyboardEvent<HTMLDivElement>) => void; // 추가된 props
+  handleEnterKeyPress: (event: React.KeyboardEvent<HTMLDivElement>) => void;
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const tags: string[] = [
-]
+const SearchBoxForTag: React.FC<SearchBoxForTagProps> = ({ setTags, handleEnterKeyPress }) => {
+  const [tagOptions, setTagOptions] = useState<string[]>([]);
 
-const SearchBoxForTag: React.FC<SearchBoxForTagProps> = ({ setTags , handleEnterKeyPress}) => {
+  const fetchTags = async () => {
+    try {
+      const response = await CustomAxios('GET', 'http://localhost:8080/api/tags/all');
+      const tags = response.data as Tag[];
+      setTagOptions(tags.map(tag => tag.name));
+    } catch (error) {
+      console.error('Failed to fetch tags:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
+
   return (
-    <Stack spacing={2} sx={{ 
-      width: '100%' ,
-      bgcolor : "white"
-      }}>
+    <Stack spacing={2} sx={{ width: '100%', bgcolor: "white" }}>
       <Autocomplete
         multiple
         id="tags-outlined"
-        options={tags.map((tag) => tag)}
+        options={tagOptions}
         getOptionLabel={(option) => option}
         onChange={(event, newValue) => setTags(newValue)}
         renderInput={(params) => (
           <TextField 
-          {...params} 
-          placeholder="태그 검색" 
-          variant="outlined" 
-          onKeyPress={handleEnterKeyPress} // Enter 키 이벤트 핸들러 추가
+            {...params} 
+            placeholder="태그 검색" 
+            variant="outlined" 
+            onKeyPress={handleEnterKeyPress}
           />
         )}
       />
@@ -38,4 +54,3 @@ const SearchBoxForTag: React.FC<SearchBoxForTagProps> = ({ setTags , handleEnter
 };
 
 export default SearchBoxForTag;
-
